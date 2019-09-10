a3374cfc208bc8a2e681cb3c626a34806fb1d922
vinaytejesh <vinaytejeshsuggurthi@gmail.com>
2019-09-09 19:35:32 -0700

    Ec2.tf

--- Ec2.tf
+++ Ec2.tf
@@ -1,23 +1,7 @@
 provider "aws" {
   region = "us-east-2"
-  access_key = "AKIAQNRZTR4KI73BYXUQ"
-  secret_key = "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"
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["029111848724"]
+  access_key = "AKIAQNRZTR4KECHBTHXA"
+  secret_key = "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"
 }
 
 resource "aws_instance" "web" {
